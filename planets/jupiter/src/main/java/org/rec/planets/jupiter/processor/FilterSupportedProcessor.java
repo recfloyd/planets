e2cf52856713918da148a0045d4a2e44c2377daf
rec floyd <rec.floyd@gmail.com>
2013-02-22 17:54:26 +0800

    filter wrap

--- planets/jupiter/src/main/java/org/rec/planets/jupiter/processor/FilterSupportedProcessor.java
+++ planets/jupiter/src/main/java/org/rec/planets/jupiter/processor/FilterSupportedProcessor.java
@@ -6,11 +6,11 @@ import org.rec.planets.jupiter.bean.CrawlContext;
 import org.rec.planets.jupiter.processor.filter.Filter;
 
 public class FilterSupportedProcessor implements CrawlProcessor {
-	private class Wrapper implements CrawlProcessor {
+	private class FilterWrappedProcessor implements CrawlProcessor {
 		private CrawlProcessor processor;
 		private Filter filter;
-		
-		private Wrapper(CrawlProcessor processor, Filter filter) {
+
+		private FilterWrappedProcessor(CrawlProcessor processor, Filter filter) {
 			this.processor = processor;
 			this.filter = filter;
 		}
@@ -19,17 +19,27 @@ public class FilterSupportedProcessor implements CrawlProcessor {
 		public void process(CrawlContext crawlContext) throws Exception {
 			filter.invoke(processor, crawlContext);
 		}
-		
+
 	}
-	
+
 	private CrawlProcessor targetProcessor;
-	
+
 	private List<Filter> filters;
 
 	@Override
 	public void process(CrawlContext crawlContext) throws Exception {
-		// TODO Auto-generated method stub
-
+		CrawlProcessor wrapped = targetProcessor;
+		for (int i = filters.size() - 1; i > -1; i--) {
+			wrapped = new FilterWrappedProcessor(wrapped, filters.get(i));
+		}
+		wrapped.process(crawlContext);
 	}
 
+	public void setTargetProcessor(CrawlProcessor targetProcessor) {
+		this.targetProcessor = targetProcessor;
+	}
+
+	public void setFilters(List<Filter> filters) {
+		this.filters = filters;
+	}
 }
