279aa541884a269fa9a3c013a4480aca480e5601
rec floyd <rec.floyd@gmail.com>
2013-04-25 16:01:30 +0800

    example子项目

---
+++ example/src/test/java/example/tao/ControllerTest.groovy
@@ -0,0 +1,18 @@
+package example.tao
+
+import org.junit.Test
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests
+
+import javax.annotation.Resource
+
+@ContextConfiguration(["classpath:example/tao/spring/applicationContext.xml"])
+class ControllerTest extends AbstractJUnit4SpringContextTests{
+	@Resource(name='controller')
+	Controller controller
+
+	@Test
+	def void test(){
+		controller.run()
+	}
+}
